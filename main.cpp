910150fee91942512aee184da019cb2c161122fa
Richard Gladman <richard@thefifthcontinent.com>
2024-08-08 15:07:32 +0100

    Set up basic plumbing for handling menu selection

--- main.cpp
+++ main.cpp
@@ -3,7 +3,11 @@
 
 #include "menu.h"
 
-using namespace std;
+void handle_add_movie();
+void handle_edit_movie();
+void handle_delete_movie();
+void handle_list_movies();
+void handle_view_movie();
 
 int main()
 {
@@ -14,6 +18,48 @@ int main()
         menu->display_main();
         selection = menu->get_selection();
 
+        switch (selection) {
+        case 'A':
+            handle_add_movie();
+            break;
+        case 'E':
+            handle_edit_movie();
+            break;
+        case 'D':
+            handle_delete_movie();
+            break;
+        case 'L':
+            handle_list_movies();
+            break;
+        case 'V':
+            handle_view_movie();
+            break;
+        }
     } while (selection != 'Q');
     return 0;
+}
+
+void handle_add_movie()
+{
+
+}
+
+void handle_edit_movie()
+{
+
+}
+
+void handle_delete_movie()
+{
+
+}
+
+void handle_list_movies()
+{
+
+}
+
+void handle_view_movie()
+{
+
 }

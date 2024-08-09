1cf831f6abbecf100acf3bc6d17e10de62129207
Richard Gladman <richard@thefifthcontinent.com>
2024-08-09 15:41:25 +0100

    Add method to get input

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <memory>
+#include <string>
+#include <ansicodes.h>
 
 #include "menu.h"
 
@@ -8,6 +10,8 @@ void handle_edit_movie();
 void handle_delete_movie();
 void handle_list_movies();
 void handle_view_movie();
+
+std::string input(std::string prompt, int min_len=0);
 
 int main()
 {
@@ -62,4 +66,21 @@ void handle_list_movies()
 void handle_view_movie()
 {
 
+}
+
+std::string input(std::string prompt, int min_len)
+{
+    std::string in {};
+
+    do {
+        std::cout << prompt << ": ";
+        std::getline(std::cin, in);
+
+        if (min_len > 0 && in.length() < min_len) {
+            std::cout << ANSICodes::RED << "Error: Input must be at least " << min_len << " characters long" << ANSICodes::RESET << std::endl;
+        }
+
+    } while (in.length() < min_len);
+
+    return in;
 }

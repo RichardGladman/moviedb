52f879a1a71699c576e2ce25f2496977479421d2
Richard Gladman <richard@thefifthcontinent.com>
2024-08-14 16:10:11 +0100

    Tweak list movies report

--- movies.cpp
+++ movies.cpp
@@ -62,16 +62,16 @@ bool Movies::remove(std::string title)
 void Movies::print(std::ostream &os) const {
 
     os << std::setw(80) << std::left << std::setfill('-') << "" << std::setfill(' ') << "\n";
-    os << std::setw(35) << std::left << "Title" << std::setw(10) << std::left << "Format"
-       << std::setw(12) << "Certificate" << std::setw(10) << "Rating" << std::setw(10) << "Running Time\n";
+    os << std::setw(35) << "Title" << std::setw(10) << std::left << "Format" << std::right
+       << std::setw(12) << "Certificate" << std::setw(10) << "Rating" << std::setw(13) << "Running Time" << "\n";
     os << std::setw(80) << std::left << std::setfill('-') << "" << std::setfill(' ') << "\n";
 
     for (const std::shared_ptr<Movie> &movie: movies) {
         os << std::setw(35) << std::left << movie->get_title();
-        os << std::setw(10) << std::left << movie->get_format();
-        os << std::setw(12) << movie->get_certificate();
+        os << std::setw(10) << movie->get_format();
+        os << std::setw(12) << std::right << movie->get_certificate();
         os << std::setw(10) << movie->get_rating();
-        os << std::setw(10) << movie->get_running_time() << "\n";
+        os << std::setw(13) << movie->get_running_time() << "\n";
     }
 }
 
